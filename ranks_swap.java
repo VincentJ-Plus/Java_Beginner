cf7834d6761fe92d2fd88eeb9f50f25968587d53
Vincent.J <56447459+VincentJ-Plus@users.noreply.github.com>
2019-11-13 17:59:44 +0800

    Create ranks_swap.java

---
+++ ranks_swap.java
@@ -0,0 +1,24 @@
+package ch6;
+
+public class Ch6_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+		System.out.println("The original array is: ");
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				System.out.print(" " + arr[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("The modified array is: ");
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				System.out.print(" " + arr[j][i]);
+			}
+			System.out.println();
+		}
+	}
+
+}
