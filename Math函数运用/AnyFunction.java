21b2c99724b00d0e95bb04bb311aece1e3ae2694
Vincent.J <56447459+VincentJ-Plus@users.noreply.github.com>
2019-11-15 23:23:12 +0800

    取最大值，最小值，绝对值等操作

---
+++ Math函数运用/AnyFunction.java
@@ -0,0 +1,12 @@
+package ch9;
+
+public class AnyFunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Larger value: " + Math.max(4,8));
+		System.out.println("Smaller value: " + Math.min(4,4.4));
+		System.out.println("Absolute value: " + Math.abs(-7));
+	}
+
+}
